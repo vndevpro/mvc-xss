fa82bf31ddcc8c9fd4d12d7d026177f6a1fe759e
juanonsoftware <juanonsoftware@github.com>
2015-10-19 15:37:14 +0700

    AllowHtml - open a security threat (xss)

--- Demo.XBanking/Demo.XBanking/Models/LineItem.cs
+++ Demo.XBanking/Demo.XBanking/Models/LineItem.cs
@@ -1,9 +1,12 @@
-﻿namespace Demo.XBanking.Models
+﻿using System.Web.Mvc;
+
+namespace Demo.XBanking.Models
 {
     public class LineItem
     {
         public int Amount { get; set; }
 
+        [AllowHtml]
         public string Message { get; set; }
     }
 }
