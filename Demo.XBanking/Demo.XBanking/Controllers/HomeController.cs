9ef2dd6a1a520ab2c9da7753b4f1f49d0b1cc790
juanonsoftware <juanonsoftware@github.com>
2015-10-26 17:45:52 +0700

    Update version

--- Demo.XBanking/Demo.XBanking/Controllers/HomeController.cs
+++ Demo.XBanking/Demo.XBanking/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Rabbit.SimpleInjectorDemo.Services;
+﻿using Demo.XBanking.Data;
+using Rabbit.SimpleInjectorDemo.Services;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Demo.XBanking.Controllers
@@ -6,15 +8,24 @@ namespace Demo.XBanking.Controllers
     public class HomeController : Controller
     {
         private readonly IListingService _listingService;
+        private readonly XBankingDbContext _dbContext;
 
         public HomeController(IListingService listingService)
         {
             _listingService = listingService;
+            _dbContext = new XBankingDbContext();
         }
 
         public ActionResult Index()
         {
             ViewBag.ListingCount = _listingService.Count();
+
+            var lastestVersion = _dbContext.Configurations.OrderByDescending(c => c.SetupDate).FirstOrDefault();
+            if (lastestVersion != null)
+            {
+                ViewBag.LastestVersion = lastestVersion.Version;
+                ViewBag.LastestVersionDate = lastestVersion.SetupDate;
+            }
 
             return View();
         }
